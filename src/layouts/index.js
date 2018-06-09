af24fbdba2a0ee153787a51d6b8027bb7509990a
hankliu5 <yliu224@ncsu.edu>
2018-06-09 16:30:09 -0400

    finished ActionsBar.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,51 +1,16 @@
 import React from "react";
-import Link from "gatsby-link";
-import g from "glamorous";
-import { css } from "glamor";
-import { rhythm } from "../utils/typography";
+import injectSheet from "react-jss";
+import { MuiThemeProvider } from "material-ui/styles";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const ListLink = props => (
-    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <Link to={props.to}>
-            {props.children}
-        </Link>
-    </li>
-);
+import theme from "../styles/theme";
+import globals from "../styles/globals";
 
-const linkStyle = css({ float: `right` });
+import { setFontSizeIncrease, setIsWideScreen } from "../state/store";
 
-export default ({ children, data }) => (
-    <g.Div
-        margin={`0 auto`}
-    >
-        <header style={{ backgroundColor: `#FFA500` }}>
-            <div style={{ margin: 'auto', maxWidth: 800, lineHeight: 5 }}>
-                <Link to='/'>
-                    <g.H3 display={`inline-block`}>
-                        {data.site.siteMetadata.title}
-                    </g.H3>
-                </Link>
-                <ul className={linkStyle}>
-                    <ListLink to="/about">About</ListLink>
-                    <ListLink to="/blog">Blog</ListLink>
-                    <ListLink to="/about-panda">About Pandas</ListLink>
-                    <ListLink to="/contact">Contact</ListLink>
-                    <ListLink to="/my-files">Source Files</ListLink>
-                </ul>
-            </div>
-        </header>
-        <div style={{ margin: '3rem auto', maxWidth: 600 }}>
-            {children()}
-        </div>
-    </g.Div>
-);
-
-export const query = graphql`
-  query LayoutQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
+import asyncComponent from "../components/common/AsyncComponent/";
+import Loading from "../components/common/Loading/";
+import Navigator from "../components/Navigator/";
+import ActionsBar from "../components/ActionsBar/";
+import InfoBar from "../components/InfoBar/";
